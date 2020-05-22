82288143e0226cb4c5228d67662b1bfd9059a110
Sjaiswal1911 <65592344+Sjaiswal1911@users.noreply.github.com>
2020-05-22 10:34:41 +0530

    Update and rename ex20.py to file_handling.py

--- python/basics/file_handling.py
+++ python/basics/file_handling.py
@@ -1,16 +1,21 @@
 from sys import argv
 
+#unpack the argv arguments
 script, input_file = argv
 
+#reading data from a file
 def print_all(f):
     print(f.read())
 
+#reading a line from the file   
 def print_a_line(line_count,f):
     print(line_count,f.readline())
 
+#taking the curosr to the start-of-file    
 def rewind(f):
     f.seek(0)
 
+#initalizing the fileIO stream    
 current_file = open(input_file)
 
 print("First let's print the whole file ./n")
