701fd930ff367955634a5b0aac54cd5f99f52ae2
Sjaiswal1911 <65592344+Sjaiswal1911@users.noreply.github.com>
2020-05-26 14:40:15 +0530

    Create rename_remove.py
    The methods to rename and remove(delete ) a file using python.

---
+++ python/IO/rename_remove.py
@@ -0,0 +1,12 @@
+# Renaming files
+# os.rename(current_file_name, new_file_name)
+import os
+
+# Rename a file from test1.txt to test2.txt
+os.rename( "test1.txt", "test2.txt" )
+
+
+# Removing files
+# os.remove(file_name)
+# deletes the file 'file_name'
+os.remove("text2.txt")
