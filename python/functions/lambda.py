e2a4d9bcfb408f9380c47508a26c9aea7c968623
Sjaiswal1911 <65592344+Sjaiswal1911@users.noreply.github.com>
2020-05-26 11:47:47 +0530

    Create lambda.py
    Anonymous functions or lambdas are implemented in the code snippet.

---
+++ python/functions/lambda.py
@@ -0,0 +1,14 @@
+# Anonymous functions
+# also known as Lambdas
+# can take any number of arguments
+# can return just one value in form of expression
+
+# syntax:
+# function_name = lambda [arg1, arg2, ......, argn] : expression
+
+# Function definition is here
+sum = lambda arg1, arg2: arg1 + arg2
+
+# Now you can call sum as a function
+print ("Value of total : ", sum( 10, 20 ))
+print ("Value of total : ", sum( 20, 20 ))
