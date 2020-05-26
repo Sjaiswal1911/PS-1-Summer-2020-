4b5ef91c5a0c8ccbaab421fed293eea277738fbe
Sjaiswal1911 <65592344+Sjaiswal1911@users.noreply.github.com>
2020-05-26 11:51:21 +0530

    Create return_statement.py
    The implementation of the return statement. The above is an example of a fruitful function.

---
+++ python/functions/return_statement.py
@@ -0,0 +1,12 @@
+# Return Types
+# the statement return() exits a function
+# The function returning a value is called Fruitful function
+def sum( arg1, arg2 ):
+   # Add both the parameters and return them."
+   total = arg1 + arg2
+   print ("Inside the function : ", total)
+   return total
+
+# Now you can call sum function
+total = sum( 10, 20 )
+print ("Outside the function : ", total )
