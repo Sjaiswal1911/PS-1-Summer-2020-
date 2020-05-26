d10cdf1177eb5ca349e9fda2b1efc95f484143b5
Sjaiswal1911 <65592344+Sjaiswal1911@users.noreply.github.com>
2020-05-26 14:49:20 +0530

    Create directory_ops.py
    The essential directory operations.

---
+++ python/IO/directory_ops.py
@@ -0,0 +1,23 @@
+# Directories in python
+
+# Make a Directory
+# os.mkdir("newdir")
+import os
+
+# Create a directory "test"
+os.mkdir("test")
+
+
+# Change Directory
+# os.chdir("newdir")
+os.chdir("/home/newdir")
+
+
+# current Working directory
+# os.getcwd
+os.getcwd()
+
+
+# Remmove directory
+# os.rmdir('dirname')
+os.rmdir( "/tmp/test"  )
