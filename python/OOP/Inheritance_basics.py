8fc1ababbdf36cb3ac844c9f184c393753573b6c
Sjaiswal1911 <65592344+Sjaiswal1911@users.noreply.github.com>
2020-05-28 10:21:23 +0530

    Create Inheritance_basics.py
    The basics of inheritance are explained in the above code implementation.

---
+++ python/OOP/Inheritance_basics.py
@@ -0,0 +1,35 @@
+# Inheritance
+# Syntax:
+# class SubClassName (ParentClass1[, ParentClass2, ...]):
+#   'Optional class documentation string'
+#   class_suite
+#Base class
+class Parent:        # define parent class
+   parentAttr = 100
+   def __init__(self):
+      print ("Calling parent constructor")
+
+   def parentMethod(self):
+      print ('Calling parent method')
+
+   def setAttr(self, attr):
+      Parent.parentAttr = attr
+
+   def getAttr(self):
+      print ("Parent attribute :", Parent.parentAttr)
+
+# Sub class declaration
+class Child(Parent): # define child class
+   def __init__(self):
+      print ("Calling child constructor")
+
+   def childMethod(self):
+      print ('Calling child method')
+
+c = Child()          # instance of child
+c.childMethod()      # child calls its method
+c.parentMethod()     # calls parent's method
+c.setAttr(200)       # again call parent's method
+c.getAttr()          # again call parent's method
+
+# The child class inherits the attributes from the parent class
