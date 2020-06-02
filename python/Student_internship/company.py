8583fd19a1c2e89f2d1fde80ffcdbfcf776c795c
Sjaiswal1911 <65592344+Sjaiswal1911@users.noreply.github.com>
2020-06-02 11:30:48 +0530

    Create company.py

---
+++ python/Student_internship/company.py
@@ -0,0 +1,9 @@
+class Company(object):
+
+    def __init__(self,name,field,MD,type):
+
+        self.name = name
+        self.field = field
+        self.MD = MD
+        slef.type = type
+        
