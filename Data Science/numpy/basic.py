923284c06bc27eb7106ddfac3d3b2a3d3a7a888a
Sjaiswal1911 <65592344+Sjaiswal1911@users.noreply.github.com>
2020-06-10 11:58:34 +0530

    basic.py
    Creating arrays in numpy

--- Data Science/numpy/basic.py
+++ Data Science/numpy/basic.py
@@ -1,1 +1,27 @@
 # Creating arrays
+import numpy as np
+
+# Creatiing an Array
+# from a list
+mylist = [1,2,3]
+n = np.array(mylist)
+print(n)
+
+# wo dimensional array
+mylist2 = ['a','b','c']
+n2 = np.array([mylist,mylist2])
+print(n2.shape)  # prints the dimension of Matrix
+print(n2)
+
+# Arange
+# numpy.arange(start,end,step)
+# creates an array [start,end) with differnce of 'step'
+n3 = np.arange(0,30,2)
+print(n3.shape)
+print(n3)
+
+
+# reshape
+# n.reshape(row,column)
+n3 = n3.reshape(3,5)
+print(n3)
