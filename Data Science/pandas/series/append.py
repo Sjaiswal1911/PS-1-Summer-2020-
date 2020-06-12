a245711f99dee90fb22758d399875fae2d2e1e98
Sjaiswal1911 <65592344+Sjaiswal1911@users.noreply.github.com>
2020-06-12 15:15:56 +0530

    Create append.py

---
+++ Data Science/pandas/series/append.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+original_sports = pd.Series({'Archery': 'Bhutan',
+                             'Golf': 'Scotland',
+                             'Sumo': 'Japan',
+                             'Taekwondo': 'South Korea'})
+cricket_loving_countries = pd.Series(['Australia',
+                                      'Barbados',
+                                      'Pakistan',
+                                      'England'],
+                                   index=['Cricket',
+                                          'Cricket',
+                                          'Cricket',
+                                          'Cricket'])
+all_countries = original_sports.append(cricket_loving_countries)
+
+# original series stays the same
+print(original_sports)
+
+# appended lists contain both
+print(all_countries)
+
+# Cricket playing countries
+print(all_countries.loc['Cricket'])
