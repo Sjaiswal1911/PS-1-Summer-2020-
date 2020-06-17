64d1e2bd415a7c05b62f49decd669b060b2f3349
Sjaiswal1911 <65592344+Sjaiswal1911@users.noreply.github.com>
2020-06-17 16:53:54 +0530

    Census.py
    operations on census.csv

---
+++ Data Science/pandas/Dataframes/consus_ops.py
@@ -0,0 +1,31 @@
+import pandas as pd
+df = pd.read_csv('census.csv')
+
+# Cleaning up data
+(df.where(df['SUMLEV']==50)
+    .dropna()
+    .set_index(['STNAME','CTYNAME'])
+    .rename(columns={'ESTIMATESBASE2010': 'Estimates Base 2010'}))
+
+# Renaming colums
+df = df[df['SUMLEV']==50]
+df.set_index(['STNAME','CTYNAME'], inplace=True)
+df.rename(columns={'ESTIMATESBASE2010': 'Estimates Base 2010'})    
+
+
+# Function to return min/max:
+import numpy as np
+def min_max(row):
+    data = row[['POPESTIMATE2010',
+                'POPESTIMATE2011',
+                'POPESTIMATE2012',
+                'POPESTIMATE2013',
+                'POPESTIMATE2014',
+                'POPESTIMATE2015']]
+    return pd.Series({'min': np.min(data), 'max': np.max(data)})
+    
+ df.apply(min_max, axis=1)
+ 
+ 
+ 
+ 
