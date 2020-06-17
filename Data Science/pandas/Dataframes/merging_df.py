bcb4ef7c288f2f3ef4d48924657eb457555e0be2
Sjaiswal1911 <65592344+Sjaiswal1911@users.noreply.github.com>
2020-06-17 16:49:29 +0530

    merging_df.py
    Explains Merging operation on dataframes

---
+++ Data Science/pandas/Dataframes/merging_df.py
@@ -0,0 +1,37 @@
+# Creating a dataframe:
+import pandas as pd
+
+df = pd.DataFrame([{'Name': 'Chris', 'Item Purchased': 'Sponge', 'Cost': 22.50},
+                   {'Name': 'Kevyn', 'Item Purchased': 'Kitty Litter', 'Cost': 2.50},
+                   {'Name': 'Filip', 'Item Purchased': 'Spoon', 'Cost': 5.00}],
+                  index=['Store 1', 'Store 1', 'Store 2'])
+print(df)
+
+#Adding a column
+df['Date'] = ['December 1', 'January 1', 'mid-May']
+df['Feedback'] = ['Positive', None, 'Negative']
+print(df)
+
+
+# New data frame
+staff_df = pd.DataFrame([{'Name': 'Kelly', 'Role': 'Director of HR'},
+                         {'Name': 'Sally', 'Role': 'Course liasion'},
+                         {'Name': 'James', 'Role': 'Grader'}])
+staff_df = staff_df.set_index('Name')
+student_df = pd.DataFrame([{'Name': 'James', 'School': 'Business'},
+                           {'Name': 'Mike', 'School': 'Law'},
+                           {'Name': 'Sally', 'School': 'Engineering'}])
+student_df = student_df.set_index('Name')
+print(staff_df.head())
+print()
+print(student_df.head())
+
+# Merging Dataframes
+pd.merge(staff_df, student_df, how='outer', left_index=True, right_index=True)
+
+# mErging again
+staff_df = staff_df.reset_index()
+student_df = student_df.reset_index()
+pd.merge(staff_df, student_df, how='left', left_on='Name', right_on='Name')
+
+
